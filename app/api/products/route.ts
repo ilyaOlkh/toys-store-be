27eae28bd09d56c89042314262ffe5f1f8a9f411
IlyaOlkh <IlyaOlkhforgit@gmail.com>
2024-09-08 18:04:17 +0300

    fix products api

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -5,8 +5,36 @@ const prisma = new PrismaClient();
 
 export async function GET() {
     try {
-        const products = await prisma.products.findMany();
-        return NextResponse.json(products);
+        const products = await prisma.products.findMany({
+            include: {
+                images: {
+                    orderBy: {
+                        id: 'asc'
+                    },
+                },
+            },
+        });
+
+        if (products.length === 0) {
+            return NextResponse.json({ error: 'No products found' }, { status: 404 });
+        }
+
+        // Формируем массив результатов
+        const results = products.map(product => ({
+            id: product.id,
+            name: product.name,
+            price: product.price,
+            discount: product.discount,
+            description: product.description,
+            stock_quantity: product.stock_quantity,
+            sku_code: product.sku_code,
+            created_at: product.created_at,
+            imageUrl: product.images.length > 0
+                ? product.images[0].image_blob
+                : '/noPhoto.png',
+        }));
+
+        return NextResponse.json(results);
     } catch (error) {
         console.error('Error fetching products:', error);
         return NextResponse.json({ error: 'Error fetching products' }, { status: 500 });
