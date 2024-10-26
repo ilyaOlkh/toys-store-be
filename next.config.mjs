fd6e99b51a1256ac6a4b96f46c3a60c636a95b49
illya.ol <illya.ol@fernir.co>
2024-10-26 15:07:09 +0300

    fix CORS

--- next.config.mjs
+++ next.config.mjs
@@ -15,7 +15,7 @@ const nextConfig = {
                     // Allows for specific methods accepted
                     {
                         key: "Access-Control-Allow-Methods",
-                        value: "GET, POST, PUT, DELETE, OPTIONS",
+                        value: "GET, POST, PUT, PATCH, DELETE, OPTIONS",
                     },
                     // Allows for specific headers accepted (These are a few standard ones)
                     {
